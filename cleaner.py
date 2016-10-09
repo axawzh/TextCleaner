c6ace405a96001d31f9b8894658d60bc409dbec0
axawzh <wuevans@hotmail.com>
2016-10-09 22:58:35 +0800

    added english word checking and a class for the pipeline

--- cleaner.py
+++ cleaner.py
@@ -1,5 +1,7 @@
 import re
-from nltk.corpus import words
+from nltk.corpus import word
+
+wordlist_path = './wordlist/words.txt'
 
 stopwords = [
     "a", 'able', 'about', 'above', 'according', 'accordingly', 'across', 'actually', 'after',
@@ -72,10 +74,9 @@ expression_pattern_alt = r'\b( ?[+−]?\w+([\.,]\w+)? ?)([+−×*/]( ?\w+(\.\d+)
 
 def count_endingfullstop(string):
     num_fullstop = 0
-    string_strip = string.strip()
-    for c in string_strip.split(' '):
-        if c.endswith('.'):
-            # count omly full stop at end of a word (full stop in middle of a word may be a float like 3.2)
+    for word in string.strip().split(' '):
+        if word.endswith('.'):
+            # count only full stop at end of a word (full stop in middle of a word may be a float like 3.2)
             num_fullstop += 1
     return num_fullstop
 
@@ -87,45 +88,40 @@ def is_formula(string):
         return True
 
 
-def replace_known(m_string):
-    if not m_string:
-        return m_string
+def replace_known(string):
     for pair in KNOWN_REPLACEMENTS:
-        m_string = m_string.replace(pair[0], pair[1])
-    return m_string
+        string = string.replace(pair[0], pair[1])
+    return string
 
 
 def remove_formula(string):
-    if not string:
-        return string
-    count = lambda l1, l2: sum([1 for x in l1 if x in l2])
-    num_comma = string.count(',')
-    num_fullstop = count_endingfullstop(string)
-    have_mathoperation = re.search(r'[+−×≤<>≥=≈]+', string)
-    if (num_comma + num_fullstop) > 0:
-        return string
-    else:
-        if not have_mathoperation:
-            if is_formula(string):
-                return '[FORMULA]'
-            else:
-                return string
+    result_str = []
+    for line in string.splitlines():
+        count = lambda l1, l2: sum([1 for x in l1 if x in l2])
+        num_comma = string.count(',')
+        num_fullstop = count_endingfullstop(string)
+        have_mathoperation = re.search(r'[+−×≤<>≥=≈]+', string)
+        if (num_comma + num_fullstop) > 0:
+            result_str.append(line)
         else:
-            return '[FORMULA]'
+            if not have_mathoperation:
+                if is_formula(string):
+                    result_str.append('[FORMULA]')
+                else:
+                    result_str.append(line)
+            else:
+                result_str.append('[FORMULA]')
+    return '\n'.join(result_str)
 
 
 def string_validation(string):
-    #   string = str(string, errors='ignore')
-    # '''
-    # if string != '\n':
-    #       return string
-    #    else:
-    #        return ''
-    #    '''
-    if string == '\n' or string.startswith(',') or string.startswith('.') or len(string.split()) == 1:
-        return ''
-    else:
-        return string
+    result_str = []
+    for line in string.splitlines():
+        if line == '\n' or line.startswith(',') or line.startswith('.') or len(line.split()) == 1:
+            result_str.append('')
+        else:
+            result_str.append(line)
+    return '\n'.join(result_str)
 
 
 def remove_stopwords(string):
@@ -164,11 +160,8 @@ def lower_firstword(string):  # lowering the capitalized first letter
     return string
 
 
-def join_brokenwords(string):  # joining the brokenwords together
-    if not string:
-        return string
-    string = re.sub(r'\b- \b', '', string)
-    return string
+def join_brokenwords(string):  # joining the broken words together
+    return re.sub(r'\b- \b', '', string)
 
 
 def remove_inlineformula(string):  # remove formulas that hide inside a sentence
@@ -195,39 +188,23 @@ def remove_inlineformula(string):  # remove formulas that hide inside a sentence
 
 # an alternative solution, remove function first, then formula
 def remove_inlineformula_alt(string):
-    if not string:
-        return string
-    have_formula = re.search(formula_pattern_alt, string)
-    have_range = re.search(range_pattern, string)
-    if have_formula:
-        string_noformula = re.sub(formula_pattern_alt, 'zzz', string)
-        string_nobracket = re.sub(r'[()]+', '', string_noformula)
-        string_noformula = re.sub(formula_pattern_alt, '[FORMULA]', string_nobracket)
-        if re.search(expression_pattern, string_noformula):
-            string_noformula = re.sub(expression_pattern, '[FORMULA]', string_noformula)
+    result_str =[]
+    for line in string.splitlines():
+        have_formula = re.search(formula_pattern_alt, line)
+        have_range = re.search(range_pattern, line)
+        if have_formula:
+            string_noformula = re.sub(formula_pattern_alt, 'zzz', line)
+            string_nobracket = re.sub(r'[()]+', '', string_noformula)
+            string_noformula = re.sub(formula_pattern_alt, '[FORMULA]', string_nobracket)
+            if re.search(expression_pattern, string_noformula):
+                string_noformula = re.sub(expression_pattern, '[FORMULA]', string_noformula)
+            if have_range:
+                string_noformula = re.sub(range_pattern, '[FORMULA]', string_noformula)
+            result_str.append(string_noformula)
         if have_range:
-            string_noformula = re.sub(range_pattern, '[FORMULA]', string_noformula)
-        return string_noformula
-    if have_range:
-        string = re.sub(range_pattern, '[FORMULA]', string)
-    return string
+            result_str.append(re.sub(range_pattern, '[FORMULA]', line))
+        return '\n'.join(result_str)
 
-    '''
-    if not string:
-        return string
-    have_function = re.search(function_pattern, string)
-    have_range = re.search(range_pattern, string)
-    if have_function or have_range:
-        string = re.sub(function_pattern, '[FORMULA]', string)
-        string = re.sub(range_pattern, '[FORMULA]', string)
-    string_nobracket = re.sub(r'[()]+', '', string)
-    have_expression = re.search(expression_pattern_alt, string_nobracket)
-    if have_expression:
-        string_noformula = re.sub(expression_pattern_alt, '[FORMULA]', string_nobracket)
-        string_noformula = re.sub(formula_pattern_alt, '[FORMULA]', string_noformula)
-        return string_noformula
-    return string
-    '''
 
 
 def remove_inlinefunction(string):  # function like 'f (n)' and 'x (1)'
@@ -252,18 +229,14 @@ def remove_figuretitles(string):
         return string
 
 
-def merge_placeholder(file):
-    result_str = ''
-    file.seek(0)
-    for line in file:
-        line = re.sub(r'(\[FORMULA\]([,.] ?)?){2,}', '[FORMULA]', line)
-        result_str += line
-    return result_str
+def merge_placeholder(string):
+    result_str = []
+    for line in string.splitlines():
+        result_str.append(re.sub(r'(\[FORMULA\]([,.] ?)?){2,}', '[FORMULA]', line))
+    return '\n'.join(result_str)
 
 
 def remove_nonascii(string):
-    if not string:
-        return string
     string = re.sub(r'[^\x00-\x7F]', '', string)
     return string
 
@@ -276,8 +249,50 @@ def merge_duplicateline(file_str):
             result_str.append(line)
     return '\n'.join(result_str)
 
-# TO DO
-# do log: show all changed lines
-# another way to call all the functions - def Cleaner
-# try this cleaner on more books: select one representative chapter from each book
-#   and run this code
+# checking each word in dictionary
+# initialization
+with open(wordlist_path, 'r') as wordlist_file:
+    english_words = set(word.strip() for word in wordlist_file)  # is .lower() needed? -check word.txt
+
+
+def is_englishword(word):
+    return word in english_words
+
+
+# checking function
+def check_english(string):
+    result_str = []
+    for line in string.strip().splitlines():
+        result_line = []
+        for word in line.strip().split(" "):
+            if is_englishword(word):
+                result_line.append(word)
+            else:
+                result_line.append('[FORMULA]')
+        ' '.join(result_line)
+        result_str.append(result_line)
+    return '\n'.join(result_str)
+
+
+class Clean():
+    methods = [string_validation,
+               remove_formula,
+               remove_inlineformula,
+               join_brokenwords,
+               replace_known,
+               remove_nonascii,
+               merge_placeholder
+               ]
+
+    def __init__(self, method_list=[]):
+        if (len(method_list) > 0):
+            self.methods = method_list
+
+    def clean(self, txt, debug=0):
+        for m in self.methods:
+            txt = m(txt)
+            if debug:
+                print('\n\n{}\n\n{}'.format(m, txt))
+        return txt
+
+
