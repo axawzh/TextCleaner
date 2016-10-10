00512a496a706f25eb096b87ee59d80d516638ab
axawzh <wuevans@hotmail.com>
2016-10-10 14:39:04 +0800

    added debug output

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ text = open(input_path + 'demofile.txt', 'r').read()
 
 cleaner = Clean()
 
-text_cleaned = cleaner.clean(text, debug=1)
+text_cleaned = cleaner.clean(text, output_path, 'demodebug.txt', debug=1)
+
 
 output_file = open(output_path + 'demooutput.txt', 'w+')
 output_file.write(text_cleaned)
