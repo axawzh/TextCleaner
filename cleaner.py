4db8595949260063a5769ebdea7ae90a84685691
axawzh <wuevans@hotmail.com>
2016-10-17 13:21:47 +0800

    added funtion to check the output with the sample answer

--- cleaner.py
+++ cleaner.py
@@ -2,68 +2,14 @@ import re
 
 wordlist_path = './word_list/words.txt'
 
-stopwords = [
-    "a", 'able', 'about', 'above', 'according', 'accordingly', 'across', 'actually', 'after',
-    'afterwards', 'again', 'against', "ain't", 'all', 'allow', 'allows', 'almost', 'alone', 'along',
-    'already', 'also', 'although', 'always', 'am', 'among', 'amongst', 'an', 'and', 'another', 'any',
-    'anybody', 'anyhow', 'anyone', 'anything', 'anyway', 'anyways', 'anywhere', 'apart', 'appear',
-    'appreciate', 'appropriate', 'are', "aren't", 'around', 'as', 'aside', 'ask', 'asking',
-    'associated', 'at', 'available', 'away', 'awfully', 'be', 'became', 'because', 'become', 'becomes',
-    'becoming', 'been', 'before', 'beforehand', 'behind', 'being', 'believe', 'below', 'beside',
-    'besides', 'best', 'better', 'between', 'beyond', 'both', 'brief', 'but', 'by', "c'mon", "c's",
-    'came', 'can', "can't", 'cannot', 'cant', 'cause', 'causes', 'certain', 'certainly', 'changes',
-    'clearly', 'co', 'com', 'come', 'comes', 'concerning', 'consequently', 'consider', 'considering',
-    'contain', 'containing', 'contains', 'corresponding', 'could', "couldn't", 'course', 'currently',
-    'definitely', 'described', 'despite', 'did', "didn't", 'different', 'do', 'does', "doesn't",
-    'doing', "don't", 'done', 'down', 'downwards', 'during', 'each', 'edu', 'eg', 'eight', 'either',
-    'else', 'elsewhere', 'enough', 'entirely', 'especially', 'et', 'etc', 'even', 'ever', 'every',
-    'everybody', 'everyone', 'everything', 'everywhere', 'ex', 'exactly', 'example', 'except', 'far',
-    'few', 'fifth', 'first', 'five', 'followed', 'following', 'follows', 'for', 'former', 'formerly',
-    'forth', 'four', 'from', 'further', 'furthermore', 'get', 'gets', 'getting', 'given', 'gives', 'go',
-    'goes', 'going', 'gone', 'got', 'gotten', 'greetings', 'had', "hadn't", 'happens', 'hardly', 'has',
-    "hasn't", 'have', "haven't", 'having', 'he', "he's", 'hello', 'help', 'hence', 'her', 'here',
-    "here's", 'hereafter', 'hereby', 'herein', 'hereupon', 'hers', 'herself', 'hi', 'him', 'himself',
-    'his', 'hither', 'hopefully', 'how', 'howbeit', 'however', "i'd", "i'll", "i'm", "i've", 'ie', 'if',
-    'ignored', 'immediate', 'in', 'inasmuch', 'inc', 'indeed', 'indicate', 'indicated', 'indicates',
-    'inner', 'insofar', 'instead', 'into', 'inward', 'is', "isn't", 'it', "it'd", "it'll", "it's",
-    'its', 'itself', 'just', 'keep', 'keeps', 'kept', 'know', 'known', 'knows', 'last', 'lately',
-    'later', 'latter', 'latterly', 'least', 'less', 'lest', 'let', "let's", 'like', 'liked', 'likely',
-    'little', 'look', 'looking', 'looks', 'ltd', 'mainly', 'many', 'may', 'maybe', 'me', 'mean',
-    'meanwhile', 'merely', 'might', 'more', 'moreover', 'most', 'mostly', 'much', 'must', 'my',
-    'myself', 'name', 'namely', 'nd', 'near', 'nearly', 'necessary', 'need', 'needs', 'neither',
-    'never', 'nevertheless', 'new', 'next', 'nine', 'no', 'nobody', 'non', 'none', 'noone', 'nor',
-    'normally', 'not', 'nothing', 'novel', 'now', 'nowhere', 'obviously', 'of', 'off', 'often', 'oh',
-    'ok', 'okay', 'old', 'on', 'once', 'one', 'ones', 'only', 'onto', 'or', 'other', 'others',
-    'otherwise', 'ought', 'our', 'ours', 'ourselves', 'out', 'outside', 'over', 'overall', 'own',
-    'particular', 'particularly', 'per', 'perhaps', 'placed', 'please', 'plus', 'possible',
-    'presumably', 'probably', 'provides', 'que', 'quite', 'qv', 'rather', 'rd', 're', 'really',
-    'reasonably', 'regarding', 'regardless', 'regards', 'relatively', 'respectively', 'right', 'said',
-    'same', 'saw', 'say', 'saying', 'says', 'second', 'secondly', 'see', 'seeing', 'seem', 'seemed',
-    'seeming', 'seems', 'seen', 'self', 'selves', 'sensible', 'sent', 'serious', 'seriously', 'seven',
-    'several', 'shall', 'she', 'should', "shouldn't", 'since', 'six', 'so', 'some', 'somebody',
-    'somehow', 'someone', 'something', 'sometime', 'sometimes', 'somewhat', 'somewhere', 'soon', 'sorry',
-    'specified', 'specify', 'specifying', 'still', 'sub', 'such', 'sup', 'sure', "t's", 'take',
-    'taken', 'tell', 'tends', 'th', 'than', 'thank', 'thanks', 'thanx', 'that', "that's", 'thats',
-    'the', 'their', 'theirs', 'them', 'themselves', 'then', 'thence', 'there', "there's", 'thereafter',
-    'thereby', 'therefore', 'therein', 'theres', 'thereupon', 'these', 'they', "they'd", "they'll",
-    "they're", "they've", 'think', 'third', 'this', 'thorough', 'thoroughly', 'those', 'though',
-    'three', 'through', 'throughout', 'thru', 'thus', 'to', 'together', 'too', 'took', 'toward',
-    'towards', 'tried', 'tries', 'truly', 'try', 'trying', 'twice', 'two', 'un', 'under',
-    'unfortunately', 'unless', 'unlikely', 'until', 'unto', 'up', 'upon', 'us', 'use', 'used', 'useful',
-    'uses', 'using', 'usually', 'value', 'various', 'very', 'via', 'viz', 'vs', 'want', 'wants', 'was',
-    "wasn't", 'way', 'we', "we'd", "we'll", "we're", "we've", 'welcome', 'well', 'went', 'were',
-    "weren't", 'what', "what's", 'whatever', 'when', 'whence', 'whenever', 'where', "where's",
-    'whereafter', 'whereas', 'whereby', 'wherein', 'whereupon', 'wherever', 'whether', 'which', 'while',
-    'whither', 'who', "who's", 'whoever', 'whole', 'whom', 'whose', 'why', 'will', 'willing', 'wish',
-    'with', 'within', 'without', "won't", 'wonder', 'would', "wouldn't", 'yes', 'yet', 'you', "you'd",
-    "you'll", "you're", "you've", 'your', 'yours', 'yourself', 'yourselves', 'zero']
-
 KNOWN_REPLACEMENTS = [
-    ('ﬁ', 'fi'), ('ﬂ', 'fl')
+    ('ﬁ', 'fi'), ('ﬂ', 'fl'), ('``', '"'), ("''", '"'), ('_', '-'), ('–', '-')
 ]
+
 formula_pattern = r'\b[^\s]* =( ?[+−]?\w+([\.,]\d+)? ?)([+−×*]( ?\w+(\.\d+)? ?))*( ?= ?[+−]?\d+(\.\d+)?)?\b'
 expression_pattern = r'\b( ?[+−]?\w+([\.,]\d+)? ?)([+−×*/]( ?\w+(\.\d+)? ?))+( ?= ?[+−]?\d+(\.\d+)?)?\b'  # formula without '='
-function_pattern = r'[ωδ\w]+ ?\([\w ωδ]+\)'
+# function_pattern = r'[ωδ\w]+ ?\([\w ωδ]+\)'
+function_pattern = r'\([\w ]+\)'
 range_pattern = r'−?(\d+|\w|∞)? [≤≥><] −?(\d+|\w|∞)'
 
 # this regex is used for remove_inlineformula_alt() function
@@ -207,13 +153,9 @@ def remove_inlineformula_alt(string):
 
 
 def remove_inlinefunction(string):  # function like 'f (n)' and 'x (1)'
-    if not string:
-        return string
     have_function = re.search(function_pattern, string)
-    have_range = re.search(range_pattern, string)
-    if have_function or have_range:
+    if have_function:
         string = re.sub(function_pattern, '[FORMULA]', string)
-        string = re.sub(range_pattern, '[FORMULA]', string)
     return string
 
 
@@ -231,7 +173,7 @@ def remove_figuretitles(string):
 def merge_placeholder(string):
     result_str = []
     for line in string.splitlines():
-        result_str.append(re.sub(r'(\[FORMULA\]([,.] ?)?){2,}', '[FORMULA]', line))
+        result_str.append(re.sub(r'(\[FORMULA\]([,.]| ?)?){2,}', '[FORMULA]', line))
     return '\n'.join(result_str)
 
 
@@ -263,7 +205,8 @@ def check_english(string):
     result_str = []
     for line in string.strip().splitlines():
         result_line = []
-        for word in line.strip().split(" "):
+        # for word in line.strip().split(" "):
+        for word in re.split(r' |-', line):
             if is_englishword(word):
                 result_line.append(word)
             else:
@@ -279,6 +222,7 @@ class Clean():
                join_brokenwords,
                replace_known,
                remove_nonascii,
+               remove_inlinefunction,
                check_english,
                merge_placeholder
                ]
@@ -295,4 +239,16 @@ class Clean():
             if debug:
                 print('\n\n{}\n\n{}'.format(m, txt), file=open(path+file_name, 'a'))
         return txt
+
+    def checkcorrected(self, txt, path='./onput/', file_name_output='checkcorrected.txt', file_name_corrected='correcteddemotext.txt'):
+        file = open(path+file_name_corrected, 'r')
+        open(path+file_name_output, 'w').truncate()
+        cleaned_line = txt.splitlines()
+        corrected_text = file.read().splitlines()
+        for i in range(len(cleaned_line)):
+            if cleaned_line[i] != corrected_text[i]:
+                print('============\nLINE ' + str(i) + '\nCLEAN RESULT: '+cleaned_line[i]\
+                      + '\nGROUND TRUTH: ' + corrected_text[i] + '\n============',\
+                      file=open(path+file_name_output, 'a'))
+
 
